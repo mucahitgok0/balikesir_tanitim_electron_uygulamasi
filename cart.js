4e0fdea395958211857088d45bea96ddb3e4fbb3
Mücahit Gök <mucahit.gok98@gmail.com>
2024-05-26 16:45:25 +0300

    gezi sepetim display
    Tıklandığında gezilecek yerlerin sepette gösterilmemesi sorunu çözüldü. İlçelere dönüldüğünde karta ekleme sorunu kaldı.

--- cart.js
+++ cart.js
@@ -2,13 +2,13 @@ let carts = document.querySelectorAll('.add-cart');
 
 let travels = [
     {
-    name: "Agia Triyada Kilisesi",
-    tag: "agiatriyadakilisesi",
-    inCart: 0
+        name: "Agia Triyada Kilisesi",
+        tag: "agiatriyadakilisesi",
+        inCart: 0
     },
     {
         name: "Çataltepe Plajı",
-        tag: "cataltepe plajı",
+        tag: "cataltepeplajı",
         inCart: 0
     },
     {
@@ -16,85 +16,79 @@ let travels = [
         tag: "ilkkursun",
         inCart: 0
     },
-]
-for(let i=0; i<carts.length; i++){
-    carts[i].addEventListener('click',() =>{
+];
+
+for (let i = 0; i < carts.length; i++) {
+    carts[i].addEventListener('click', () => {
         cartNumbers(travels[i]);
-    })
+    });
 }
-function onLoadCartNumbers(){
+
+function onLoadCartNumbers() {
     let travelNumbers = localStorage.getItem('cartNumbers');
-    if(travelNumbers){
+    if (travelNumbers) {
         document.querySelector('.cart span').textContent = travelNumbers;
     }
 }
 
-function cartNumbers(travels) {
-            console.log("tıklandığında verilerin localstoragee kaydedildiğini gösteriyor",travels);
-            let travelNumbers = localStorage.getItem('cartNumbers');
-       
-        
-        travelNumbers = parseInt(travelNumbers);
-        
-        if( travelNumbers){
-            if(travels.inCart != 1){
-            localStorage.setItem('cartNumbers',travelNumbers+1);//kart numarasını localstorage a yazdır
-            document.querySelector('.cart span').textContent = travelNumbers+1;//kart numarasını ekrana yazdır
-            }
-        }
-        else{
-            localStorage.setItem('cartNumbers',1)//lcoal storage da kart numarası 1
-            document.querySelector('.cart span').textContent = 1;//ekranda kart numarası 1
-            
-        }
-        //set gönderme
-        setItems(travels);
-        
-}
+function cartNumbers(travel) {
+    console.log("tıklandığında verilerin localstoragee kaydedildiğini gösteriyor", travel);
+    let travelNumbers = localStorage.getItem('cartNumbers');
+    travelNumbers = parseInt(travelNumbers);
 
-function setItems(travels)
-{
-    let cartItems = localStorage.getItem('travelsInCart');
-    travels.inCart = 1;
-    cartItems = JSON.parse(cartItems);
-    if(cartItems != null){
-        if(travels.inCart == 0){
-            if(cartItems[travels.tag] == undefined){
-                cartItems = {
-                    ...cartItems,
-                    [travels.tag]: travels
-                }
+    if (travelNumbers) {
+        if (travel.inCart === 0) {
+            localStorage.setItem('cartNumbers', travelNumbers + 1);
+            document.querySelector('.cart span').textContent = travelNumbers + 1;
         }
-        }}
-    else{
-        cartItems = {
-            [travels.tag]: travels
-        }
+    } else {
+        localStorage.setItem('cartNumbers', 1);
+        document.querySelector('.cart span').textContent = 1;
     }
-    localStorage.setItem("travelsInCart",JSON.stringify
-    (cartItems));
+
+    setItems(travel);
 }
 
-    
+function setItems(travel) {
+    let cartItems = localStorage.getItem('travelsInCart');
+    cartItems = JSON.parse(cartItems);
 
+    if (cartItems != null) {
+        if (cartItems[travel.tag] === undefined) {
+            cartItems = {
+                ...cartItems,
+                [travel.tag]: travel
+            };
+        }
+        cartItems[travel.tag].inCart += 1;
+    } else {
+        travel.inCart = 1;
+        cartItems = {
+            [travel.tag]: travel
+        };
+    }
 
-function displayCart(){
+    localStorage.setItem("travelsInCart", JSON.stringify(cartItems));
+}
+
+function displayCart() {
     let cartItems = localStorage.getItem("travelsInCart");
     cartItems = JSON.parse(cartItems);
     let travelContainer = document.querySelector(".travels");
-    console.log(cartItems);
-    if(cartItems && travelContainer){
+
+    if (cartItems && travelContainer) {
         travelContainer.innerHTML = '';
-        Object.values(cartItems).map(item => {
-            travelContainer.innerHTML +=  `
+        Object.values(cartItems).forEach(item => {
+            travelContainer.innerHTML += `
             <div class="travel">
-            <span>${item.name}</span>
-            <img src="./images/${item.tag}.jpg">
-            <ion-icon name="trash-outline"></ion-icon>
+                <span>${item.name}</span>
+                <img src="./images/${item.tag}.jpg">
+                <ion-icon name="trash-outline"></ion-icon>
             </div>
-            `
+            `;
         });
     }
 }
+
 onLoadCartNumbers();
 displayCart();
